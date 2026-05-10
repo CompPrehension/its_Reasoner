cfc3fce1bc83312d2e4773c401fe471b895e2dee
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2026-05-10 15:54:55 +0300

    [fix]: Исправление проблемы обработки новых процедур, работающих с выражениями

--- src/main/kotlin/its/reasoner/procedures/DebugObjectPrintImpl.kt
+++ src/main/kotlin/its/reasoner/procedures/DebugObjectPrintImpl.kt
@@ -1,18 +1,15 @@
 package its.reasoner.procedures
 
+import its.model.definition.ObjectRef
 import its.model.definition.procedures.DebugObjectPrintDef
-import its.model.expressions.Operator
 import its.reasoner.LearningSituation
-import its.reasoner.operators.DomainInterpreterReasoner
 
 class DebugObjectPrintImpl(procedure: DebugObjectPrintDef,
                          learningSituation: LearningSituation
 ) : ProcedureImpl<DebugObjectPrintDef>(procedure, learningSituation) {
 
     override fun process(evaluatedArguments: List<Any>) {
-        val obj = evaluatedArguments[0] as Operator;
-        val reasoner = DomainInterpreterReasoner(accessLearningSituation()!!)
-        val result = obj.use(reasoner);
-        ReasonerOutput.println(result);
+        val obj = evaluatedArguments[0] as ObjectRef;
+        ReasonerOutput.println(obj);
     }
 }
